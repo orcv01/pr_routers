1ca37341bea7c4c9f280aae9fc710a3d05c0bc60
UsuarioM <orcv01@gmail.com>
2023-09-28 14:25:00 +0200

    Upload pr_routers_v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+//Importamos todas las paginas
+import Home from './pages/Home';
+import Heroe from './pages/Heroe';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+//Importamos los 3 componentes necesarios para enrutar la aplicación
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import './index.css';
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter basename="/">
+      <Routes>
+        
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="/Heroe" element={<Heroe />} />
+            <Route path="/About" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>    
+  </React.StrictMode>
 )
---
+++ src/pages/Heroe/Heroe.jsx
@@ -0,0 +1,7 @@
+import "./Heroe.css";
+
+const Heroe = () => {
+  return <main>Heroe</main>;
+};
+
+export default Heroe;
